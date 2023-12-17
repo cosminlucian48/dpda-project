11ae20eddd60776f4170b1fb78c84d58ed346b58
cosminlucian48 <cbejan1908@gmail.com>
2023-12-17 11:52:44 +0200

    organize client code

--- src/Client.java
+++ src/Client.java
@@ -5,115 +5,99 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
-        String nodeHost = "localhost";
-        Socket socket;
-        int nodePort;
-        PrintWriter out;
-        BufferedReader in;
-        String message, serverResponse;
+    private static final String DEFAULT_NODE_HOST = "localhost";
+    private static final int DEFAULT_NODE_PORT = 12345;
 
+    public static void main(String[] args) {
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
         try{
             while(true){
-                System.out.println("\nChoose an option:");
-                System.out.println("Note: By default if port 12345 is empty, that will be the first port a node will try to use.");
-                System.out.println("[topology <NODE_PORT>]. Show topology from node perspective.");
-                System.out.println("[service <NODE_PORT> <SERVICE>]. Run service on node.");
-                System.out.println("[exit]. Exit\n");
-                System.out.print(">>");
-
+                displayMenu();
                 String[] choice = userInput.readLine().split(" ");
-                message = choice[0];
-                nodePort = Integer.valueOf(choice[1]);
-                
-                switch (choice[0]) {
-                    case "topology":
-                        if (choice.length<2){
-                            System.out.println("To few parameters were given...");
-                            System.out.println("Press any key to continue...");
-                            userInput.readLine();
-                            break;
-                        }
-                        //System.out.println(String.format("Trying to connect to %s:%s", nodeHost, nodePort));
-                        try {
-                            socket = new Socket(nodeHost, nodePort);
-                        } catch (Exception e) {
-                            System.out.println("Could not establish connection to the requested node...");
-                            System.out.println("Press any key to continue...");
-                            userInput.readLine();
-                            break;
-                        }
-                        
-                        out = new PrintWriter(socket.getOutputStream(), true);
-                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                        //System.out.println(String.format("Connected to %s:%s", nodeHost, nodePort));
-
-                        out.println(message);
-                        System.out.println("Sent to node: " + message);
-
-                        String [] res = in.readLine().split("\\|");
-                        System.out.println("Received from node:\n" + String.join("\n", res));
-                        System.out.println("Press any key to continue...");
-                        userInput.readLine();
-
-                        in.close();
-                        out.close();
-                        socket.close();
-                        break;
-
-                    case "service":
-                        if (choice.length<3){
-                            System.out.println("To few parameters were given...");
-                            System.out.println("Press any key to continue...");
-                            userInput.readLine();
-                            break;
-                        }
-                        String service = choice[2];
-                        
-                        //System.out.println(String.format("Trying to connect to %s:%s", nodeHost, nodePort));
-                        
-                        try {
-                            socket = new Socket(nodeHost, nodePort);
-                        } catch (Exception e) {
-                            System.out.println("Could not establish connection to the requested node...");
-                            System.out.println("Press any key to continue...");
-                            userInput.readLine();
-                            break;
-                        }
-                        out = new PrintWriter(socket.getOutputStream(), true);
-                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                        //System.out.println(String.format("Connected to %s:%s", nodeHost, nodePort));
-
-                        message = message + " " + service;
-                        out.println(message);
-                        System.out.println("Sent to node: " + message);
-
-                        serverResponse = in.readLine();
-                        System.out.println("Received from node: " + serverResponse);
-                        System.out.println("Press any key to continue...");
-                        userInput.readLine();
-
-                        in.close();
-                        out.close();
-                        socket.close();
-                        break;
-
-                    case "exit":
-                        System.out.println("Press any key to continue...");
-                        userInput.readLine();
-                        return;
-
-                    default:
-                        System.out.println("The command you entered does not exist...");
-                        break;
-                }
+                processChoice(choice, userInput);
             }
-
         }catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("\nChoose an option:");
+        System.out.println("Note: By default, if port 12345 is empty, that will be the first port a node will try to use.");
+        System.out.println("[topology <NODE_PORT>]. Show topology from node perspective.");
+        System.out.println("[service <NODE_PORT> <SERVICE>]. Run service on node.");
+        System.out.println("[exit]. Exit\n");
+        System.out.print(">>");
+    }
+
+    private static void processChoice(String[] choice, BufferedReader userInput) throws IOException {
+        String message = choice[0];
+        int nodePort;
+        try{
+            nodePort = (choice.length > 1) ? Integer.parseInt(choice[1]) : DEFAULT_NODE_PORT;
+        }catch(Exception e){
+            System.out.println("Bad parameters...");
+            return;
+        }
+
+        switch (message) {
+            case "topology":
+                handleTopologyRequest(nodePort, userInput);
+                break;
+            case "service":
+                if (choice.length > 2) {
+                    String service = choice[2];
+                    handleServiceRequest(nodePort, service, userInput);
+                } else {
+                    System.out.println("Too few parameters were given...");
+                }
+                break;
+            case "exit":
+                System.out.println("Press any key to continue...");
+                userInput.readLine();
+                System.exit(0);
+            default:
+                System.out.println("The command you entered does not exist...");
+                break;
+        }
+    }
+
+    private static void handleTopologyRequest(int nodePort, BufferedReader userInput) throws IOException {
+        try (Socket socket = new Socket(DEFAULT_NODE_HOST, nodePort);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            out.println("topology");
+            System.out.println(String.format("Sent to node %s: topology", nodePort));
+
+            String[] res = in.readLine().split("\\|");
+            System.out.println("Received from node:\n" + String.join("\n", res));
+            System.out.println("Press any key to continue...");
+            userInput.readLine();
+        } catch (IOException e) {
+            System.out.println("Could not establish connection to the requested node...");
+            System.out.println("Press any key to continue...");
+            userInput.readLine();
+        }
+    }
+
+    private static void handleServiceRequest(int nodePort, String service, BufferedReader userInput) throws IOException {
+        try (Socket socket = new Socket(DEFAULT_NODE_HOST, nodePort);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            String message = "service " + service;
+            out.println(message);
+            System.out.println("Sent to node: " + message);
+
+            String serverResponse = in.readLine();
+            System.out.println("Received from node: " + serverResponse);
+            System.out.println("Press any key to continue...");
+            userInput.readLine();
+        } catch (IOException e) {
+            System.out.println("Could not establish connection to the requested node...");
+            System.out.println("Press any key to continue...");
+            userInput.readLine();
         }
     }
 }
